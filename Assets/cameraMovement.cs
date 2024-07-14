3adaefd2b597aca1beff1cbc7b4e9633bfedf9eb
Muhammad Ahsan <ahsanirfan961@gmail.com>
2024-07-14 19:10:30 +0500

    Issue # 16

--- Assets/cameraMovement.cs
+++ Assets/cameraMovement.cs
@@ -9,20 +9,20 @@ public class cameraMovement : MonoBehaviour
     [SerializeField] private float smoothSpeed = 0.125f;
     private bool move = false;
     private const float StopThreshold = 0.2f;
-    
+
     public void MoveCamera()
     {
         move = true;
     }
-    
+
     void LateUpdate()
     {
         if (!move) return;
-    
+
         Vector3 desiredPosition = new Vector3(transform.position.x, followObject.transform.position.y + offset.y, transform.position.z);
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
-    
+
         if (Mathf.Abs(transform.position.y - desiredPosition.y) < StopThreshold)
         {
             move = false;
--- Assets/setReferences.cs
+++ Assets/setReferences.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class setReferences : MonoBehaviour
 {
     public Slingshot slingshot;
-    public cameraMovement cameraMovement;
+    public cameraMovement cameraMovement; 
     worldShift worldShift;
     public hookSpawner hookSpawner;
     void Start()
